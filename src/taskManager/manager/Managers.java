f2fbcd80f6e39d5abf25bc425d062b93df1bf7c7
Toolll1 <d-e-n-i-s-ne@yandex.ru>
2022-11-19 11:30:23 +0300

    что-то типо того?

--- src/taskManager/manager/Managers.java
+++ src/taskManager/manager/Managers.java
@@ -1,11 +1,16 @@
 package taskManager.manager;
 
-public class Managers {
-    public static TaskManager getInMemoryTaskManager(HistoryManager historyManager) {
-        return new InMemoryTaskManager(historyManager);
-    }
+import taskManager.manager.managerForHistory.HistoryManager;
+import taskManager.manager.managerForHistory.InMemoryHistoryManager;
+import taskManager.manager.managerForTask.InMemoryTaskManager;
+import taskManager.manager.managerForTask.TaskManager;
 
+public class Managers {
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
+    }
+
+    public static TaskManager getDefaultTask() {
+        return new InMemoryTaskManager();
     }
 }
